59e1fd577810a751527a8864a3c8959b7ade305d
levashovIM <i.Levashov0218@gmail.com>
2023-12-06 18:53:47 +0700

    comment some str

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,14 +19,14 @@ app = FastAPI(
     redoc_url = None
 )
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000"
-]
+#origins = [
+    #"http://localhost",
+    #"http://localhost:3000"
+#]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    #allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
